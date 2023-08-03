6d0b972e07476fa40b118afa4881ee1a577903f8
Vlad <Vlad Bakh>
2023-08-03 15:36:10 +0300

    Initial Commit

---
+++ MapAppUA/MapAppUAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppUAApp.swift
+//  MapAppUA
+//
+//  Created by user on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppUAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapAppUA/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  MapAppUA
+//
+//  Created by user on 03.08.2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
